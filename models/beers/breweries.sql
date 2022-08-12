fe8e4f0510fe6ef4c66d39512fe3ea4ac3a95edb
VFisa <vfisa.stuff@gmail.com>
2022-08-11 17:25:40 -0700

    Update for Keboola environment

--- models/beers/breweries.sql
+++ models/beers/breweries.sql
@@ -10,4 +10,4 @@ SELECT
   trim("state")           AS brewery_state,
   'USA'                 AS brewery_country
 FROM
-  {{ source('in.c-dbt_beer', 'seed_breweries') }}
+  {{ source('in.c-dbt_beer_seed', 'seed_breweries') }}
