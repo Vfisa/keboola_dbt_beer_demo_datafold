8aa10d9dea87b916b4bbb5d2a769ec78e6b33896
VFisa <vfisa.stuff@gmail.com>
2022-10-14 21:45:35 -0700

    beers.sql fix

--- models/beers/beers.sql
+++ models/beers/beers.sql
@@ -1,4 +1,3 @@
-
 {{ config(
     materialized="table"
 ) }}
@@ -7,11 +6,11 @@ SELECT
   "id"            AS beer_id,
   TRIM("name")    AS beer_name,
   "style"         AS beer_style,
-  "abv"           AS abv,
-  "ibu"           AS ibu,
+  TRY_TO_NUMERIC("abv",14,8)           AS abv,
+  TRY_TO_NUMERIC("ibu",14,8)           AS ibu,
   CASE 
-       WHEN "ibu" <= 50 THEN 'Malty'
-       WHEN "ibu" > 50 THEN 'Hoppy'
+       WHEN ibu <= 50 THEN 'Malty'
+       WHEN ibu > 50 THEN 'Hoppy'
    END AS bitterness,
   "brewery_id"    AS brewery_id,
   "ounces"        AS ounces
